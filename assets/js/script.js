8729a9a29dc108b217ae941388898a02498db020
Soprano <antfar67@gmail.com>
2022-10-21 23:05:30 -0700

    added score and high score storage functionality, outlined high score page, styled start and finish boxes with css

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,6 @@ var questions = [
     ]
   },
   {
-
     question: "Arrays in JavaScript can be used to store:",
     answer: "all of the above",
     options: [
@@ -51,10 +50,7 @@ var questions = [
       "console.log()"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
   //   {
-  //   numb: 6,
   //   question: "Your Question is Here",
   //   answer: "Correct answer of the question is here",
   //   options: [
@@ -71,28 +67,38 @@ var startButton = document.querySelector(".start");
 var startBox = document.querySelector(".start-box");
 var quizBox = document.querySelector(".quiz-box");
 var finishBox = document.querySelector(".finish-box");
-var timer = document.querySelector("#timer")
+var highScoreBox = document.querySelector(".high-score-box");
+var timer = document.querySelector("#timer");
+var highScoreLink = document.querySelector('#high-score');
 var questionsContainer = document.querySelector(".questions");
 var validText = document.querySelector(".validity-text");
-var validTextBorder = document.querySelector(".validity")
-var finalScore = document.querySelector(".final-score")
-var currentQuestion = 0;
+var validTextBorder = document.querySelector(".validity");
+var finalScore = document.querySelector(".final-score");
+var initialInput = document.querySelector(".initial-input");
+var submitButton = document.querySelector(".submit-button");
 var secondsLeft = 45;
+var currentQuestion = 0;
+var initialScoreArray;
+var highScoreArray;
+var score;
 
 function setTime() {                          //Function for timer
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
+
     secondsLeft--;
     timer.textContent = "Time: " + secondsLeft;
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
+      score = 0;
       quizBox.classList.add("hidden");
-      finish-box.classList.remove("hidden");
+      finishBox.classList.remove("hidden");
     }
-    else if(secondsLeft <= 0){
+    else if (secondsLeft <= 0) {
       secondsLeft = 0;
+      score = 0;
       timer.textContent = "Time: " + secondsLeft;
       quizBox.classList.add("hidden");
-      finish-box.classList.remove("hidden");
+      finishBox.classList.remove("hidden");
     }
   }, 1000);
 }
@@ -101,11 +107,7 @@ startButton.addEventListener("click", function () {
   startBox.classList.add("hidden");
   quizBox.classList.remove("hidden");
   // setTime();
-
-
 });
-
-
 
 questionsContainer.addEventListener("click", function (event) {
 
@@ -115,13 +117,13 @@ questionsContainer.addEventListener("click", function (event) {
     console.log(event.target.innerText);
     console.log(questions[currentQuestion].answer);
     //TODO: Check validity and save to validity-text
-    if (event.target.innerText === questions[currentQuestion].answer){
+    if (event.target.innerText === questions[currentQuestion].answer) {
       console.log('correct answer');
       validTextBorder.classList.add('validity-result');
       validText.innerText = 'Correct!';
       currentQuestion++;
     }
-    else if(event.target !== questions[currentQuestion].answer){
+    else if (event.target !== questions[currentQuestion].answer) {
       console.log('incorrect answer');
       validTextBorder.classList.add('validity-result');
       validText.innerText = 'Incorrect!';
@@ -132,15 +134,16 @@ questionsContainer.addEventListener("click", function (event) {
     if (currentQuestion != questions.length - 1) { // 0v2 1v2 2v2
       // currentQuestion++;
       renderQuestion();
-    } 
+    }
     else {
+      score = secondsLeft;
+      console.log(score)
       quizBox.classList.add("hidden");
       finishBox.classList.remove("hidden");
     }
     // currentQuestion++;
   }
 });
-
 
 function renderQuestion() {
   console.log("Rendered")
@@ -154,5 +157,33 @@ function renderQuestion() {
 
 renderQuestion();
 
+function logScore(event) {
+  // Prevent default action
+  event.preventDefault();
+  console.log(event);
+  initialScoreArray = [initialInput.value, score];
+  console.log("this is the initalScoreArray: " + initialScoreArray);
+  highScoreArray = JSON.parse(localStorage.getItem('highScore')) ?? [];
+  highScoreArray.push(initialScoreArray);
+  console.log("this is the current highScoreArray: " + highScoreArray);
+  // add json string of high score array to local storage
+  localStorage.setItem("highScore", JSON.stringify(highScoreArray));
+  //  work on renderHighScores
 
+  // const highScores = JSON.parse(highScoreString) ?? []; --> example that acccounts for empty array return
+}
 
+// Add listener to submit element
+submitButton.addEventListener("click", logScore);
+
+function renderHighScores() {
+  // hide finish-box, unhide high-score box
+  finishBox.classList.add("hidden");
+  var highScoreList = JSON.parse(localStorage.getItem("highScore"));
+  // write for loop to loop thru high scores and add concatenated index values of each item 
+  // into li elements within ordered list
+
+}
+
+highScoreLink.addEventListener("click", renderHighScores);
+// renderHighScores();
